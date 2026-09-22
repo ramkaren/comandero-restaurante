26a561befd8e9a5e9ec073b4e5de892867543420
sarahiortiz01 <L211080082@iztapalapa.tecnm.mx>
2026-09-21 19:28:08 -0600

    Primer commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi primera página Laravel</title>
+</head>
+<body>
+
+    <h1>¡Hola! 👋</h1>
+
+    <h2>Mi primera página en Laravel</h2>
+
+    <p>Laravel está funcionando correctamente.</p>
+
+</body>
+</html>
